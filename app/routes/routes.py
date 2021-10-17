205adfc4f85eac7c295b160b3ac9a6efa3bff817
tennin12 <bassembenyhia12@gmail.com>
2021-10-18 00:05:15 +0100

    update

--- app/routes/routes.py
+++ app/routes/routes.py
@@ -1,8 +1,8 @@
 """modul contain all route"""
 from flask import Flask, render_template
 from app.app import app
-
 from models import User,RegisterForm
+
 @app.route('/')
 def home_page():
     return render_template('index.html')
@@ -12,6 +12,6 @@ def register_page():
     form = RegisterForm()
     return render_template('register.html', form=form)
 
-@app.route('/base')
-def base():
-    return render_template('base.html')
+@app.route('/login')
+def login():
+    return render_template('login.html')
