b1aba1db9d2eca8a51f24691807f0bf1b6e697e4
ahmedzitouni586 <zitouniahmed595@gmail.com>
2021-10-22 10:32:09 -0700

    updates

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -4,9 +4,9 @@ var big_wrapper;
 var hamburger_menu;
 
 function declare() {
-    toggle_btn = document.querySelector(".toggle-btn");
-    big_wrapper = document.querySelector(".big-wrapper");
-    hamburger_menu = document.querySelector(".hamburger-menu");
+  toggle_btn = document.querySelector(".toggle-btn");
+  big_wrapper = document.querySelector(".big-wrapper");
+  hamburger_menu = document.querySelector(".hamburger-menu");
 }
 
 const main = document.querySelector("main");
@@ -16,78 +16,37 @@ declare();
 let dark = false;
 
 function toggleAnimation() {
-    // Clone the wrapper
-    dark = !dark;
-    let clone = big_wrapper.cloneNode(true);
-    if (dark) {
-        clone.classList.remove("light");
-        clone.classList.add("dark");
-    } else {
-        clone.classList.remove("dark");
-        clone.classList.add("light");
-    }
-    clone.classList.add("copy");
-    main.appendChild(clone);
+  // Clone the wrapper
+  dark = !dark;
+  let clone = big_wrapper.cloneNode(true);
+  if (dark) {
+    clone.classList.remove("light");
+    clone.classList.add("dark");
+  } else {
+    clone.classList.remove("dark");
+    clone.classList.add("light");
+  }
+  clone.classList.add("copy");
+  main.appendChild(clone);
 
-    document.body.classList.add("stop-scrolling");
+  document.body.classList.add("stop-scrolling");
 
-    clone.addEventListener("animationend", () => {
-        document.body.classList.remove("stop-scrolling");
-        big_wrapper.remove();
-        clone.classList.remove("copy");
-        // Reset Variables
-        declare();
-        events();
-    });
+  clone.addEventListener("animationend", () => {
+    document.body.classList.remove("stop-scrolling");
+    big_wrapper.remove();
+    clone.classList.remove("copy");
+    // Reset Variables
+    declare();
+    events();
+  });
 }
 
 function events() {
-    toggle_btn.addEventListener("click", toggleAnimation);
-    hamburger_menu.addEventListener("click", () => {
-        big_wrapper.classList.toggle("active");
-    });
+  toggle_btn.addEventListener("click", toggleAnimation);
+  hamburger_menu.addEventListener("click", () => {
+    big_wrapper.classList.toggle("active");
+  });
 }
 
 events();
-$('.form').find('input, textarea').on('keyup blur focus', function(e) {
 
-    var $this = $(this),
-        label = $this.prev('label');
-
-    if (e.type === 'keyup') {
-        if ($this.val() === '') {
-            label.removeClass('active highlight');
-        } else {
-            label.addClass('active highlight');
-        }
-    } else if (e.type === 'blur') {
-        if ($this.val() === '') {
-            label.removeClass('active highlight');
-        } else {
-            label.removeClass('highlight');
-        }
-    } else if (e.type === 'focus') {
-
-        if ($this.val() === '') {
-            label.removeClass('highlight');
-        } else if ($this.val() !== '') {
-            label.addClass('highlight');
-        }
-    }
-
-});
-
-$('.tab a').on('click', function(e) {
-
-    e.preventDefault();
-
-    $(this).parent().addClass('active');
-    $(this).parent().siblings().removeClass('active');
-
-    target = $(this).attr('href');
-
-    $('.tab-content > div').not(target).hide();
-
-    $(target).fadeIn(600);
-
-});
